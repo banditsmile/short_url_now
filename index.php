9fed3510372dd88c79d3e435341d9350cfd6b068
banditsmile <banditsmile@qq.com>
2019-06-27 20:53:49 +0800

    init

--- index.php
+++ index.php
@@ -6,51 +6,68 @@ class short_url_now
     protected $len = 6;
     //短url生命周期
     protected $ttl = 86400;
-    //
-    protected $dbFile = 'short_url.db';
+    //当前请求地址信息
+    protected $urlInfo;
 
     public function index()
     {
-        $this->apcu();
+        $this->urlInfo = parse_url($_SERVER['REQUEST_URI']);
+        $code = ltrim($this->urlInfo['path'], '/');
+        if (is_numeric($code)) {
+            $this->redirect($code);
+        }
+
+        if (method_exists($this, $code)) {
+            $this->$code();
+        }
+        header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
     }
 
-
-    public function apcu()
+    protected function redirect($code)
     {
-        apcu_add('aa','bbb');
-        $b = apcu_fetch('aa');
-        var_dump($b);
+        $url = apcu_fetch($code);
+        if (!empty($url)) {
+            header("Location: {$url}", false, 301);
+        }
+        header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
     }
-    public function add($url, $user='nobody')
+
+    public function add( )
     {
-        $urlHash = md5($url);
-        $db = new SQLite3($this->dbFile);
+        $url = $_SERVER['QUERY_STRING'];
+        $code = $this->code();
+        while (apcu_add($code, $url, $this->ttl) ===false) {
+            $code = $this->code();
+        }
+        echo  $code;
     }
 
-    public function sqlite()
+
+
+    /**
+     * 由短地址获取原来地址
+     *
+     * @param $code
+     *
+     * @return mixed
+     */
+    protected function get($code)
     {
-        $db = new SQLite3($this->dbFile);
-        $db->exec("INSERT INTO foo (bar) VALUES ('This is a test')");
-
-        $result = $db->query('SELECT bar FROM foo');
-        var_dump($result->fetchArray());
+        return apcu_fetch($code);
     }
 
-    public function install()
+    /**
+     * 生成便于记忆的短地址
+     *
+     * @return string
+     */
+    protected function code()
     {
-        $db = new SQLite3($this->dbFile);
-        $tableSql = 'CREATE TABLE main.url (
-                  id         INTEGER PRIMARY KEY  AUTOINCREMENT NOT NULL,
-                  code       CHAR(20)                           NOT NULL,
-                  hash       CHAR(35)                           NOT NULL,
-                  url        VARCHAR(255)                       NOT NULL,
-                  start_time INTEGER                            NOT NULL,
-                  end_time   INTEGER                            NOT NULL
-                );';
-
-        $db->exec($tableSql);
+        $min= 100000;
+        $max = 999999;
+        $code = rand($min, $max);
+        return (string)$code;
     }
-
 }
 
 (new short_url_now())->index();
